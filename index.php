5210f28247b050360e66377f915ea277020cabe3
georgetassiano <george.melo7@gmail.com>
2016-07-06 01:27:01 -0300

    Inicio da página e cabeçalho

---
+++ index.php
@@ -0,0 +1,14 @@
+/**
+* Created by PhpStorm.
+* User: George Tassiano
+* Date: 06/07/2016
+* Time: 00:44
+*/
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title><?php print $title_name ?></title>
+    <meta charset="utf-8">
+    <link href="<?php print $?>"
+</head>
+<body>
